d4088ee6023755b87d90da76f82617b3c0378193
Mao Li <63109264+maoli67660@users.noreply.github.com>
2022-05-30 23:20:57 +0100

    Update 3-Rotate_Array.py
    add one line pass

--- top-interview-questions/Easy/1-Array/3-Rotate_Array.py
+++ top-interview-questions/Easy/1-Array/3-Rotate_Array.py
@@ -27,7 +27,8 @@ def rotate(nums, k):
     roatate_arr(nums,0,len(nums)-1)
     roatate_arr(nums,0,k%len(nums)-1)
     roatate_arr(nums,k%len(nums),len(nums)-1)
-
+    
+# 一行过 nums[:] = nums[::-1][:k%len(nums)][::-1] + nums[::-1][k%len(nums):][::-1]
 
 if __name__ == '__main__':
     assert rotate(nums=[1, 2, 3, 4, 5, 6, 7], k=3) == [5, 6, 7, 1, 2, 3, 4]
