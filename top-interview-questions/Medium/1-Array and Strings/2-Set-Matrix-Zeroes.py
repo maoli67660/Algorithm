121c38e59d0f073732d336b9d93887b87e06bdab
Mao Li <63109264+maoli67660@users.noreply.github.com>
2022-06-16 13:35:27 +0100

    Update 2-Set-Matrix-Zeroes.py

--- top-interview-questions/Medium/1-Array and Strings/2-Set-Matrix-Zeroes.py
+++ top-interview-questions/Medium/1-Array and Strings/2-Set-Matrix-Zeroes.py
@@ -1,3 +1,7 @@
+'''
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+'''
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
